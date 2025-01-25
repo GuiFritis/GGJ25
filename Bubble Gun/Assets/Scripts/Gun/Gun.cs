5306c6cb68fa46d4be71c0a8891da3307d92def3
Arthur-lc <arthurlima.costa@estudante.ufjf>
2025-01-25 12:24:53 -0300

    triangulo

--- Bubble Gun/Assets/Scripts/Gun/Gun.cs
+++ Bubble Gun/Assets/Scripts/Gun/Gun.cs
@@ -7,16 +7,19 @@ public class Gun : MonoBehaviour
     [SerializeField] private float maxAngle;
     [SerializeField] private float wobleSpeed;
     [SerializeField] private Transform target;
+    [SerializeField] private Transform gunPivot;
 
     void Update()
     {
         Vector3 difference = target.position - transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f,0f,rotationZ);
+        
         float angle = Mathf.Sin(Time.time * wobleSpeed) * maxAngle;
         angle += transform.rotation.z;
         rotationZ += angle;
 
-        transform.rotation = Quaternion.Euler(0f,0f,rotationZ);
+        gunPivot.rotation = Quaternion.Euler(0f,0f,rotationZ);
 
     }
 }
