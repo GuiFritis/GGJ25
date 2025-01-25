7ec3088852fae27428609665dc4f4305559c4ad2
Lorran Sutter <sutterlorran@gmail.com>
2025-01-25 11:20:46 -0300

    health | collision

---
+++ Bubble Gun/Assets/Scripts/Player/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth = 12;
+    private int _currentHealth;
+
+    public Action OnDamage;
+    public Action OnKilled;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        OnDamage?.Invoke();
+
+        if(_currentHealth <= 0)
+        {
+            OnKilled?.Invoke();
+        }
+    }
+}
