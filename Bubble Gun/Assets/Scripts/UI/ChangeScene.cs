f01ac57648ebc99fa612f21f783f59ae9e4daf06
Lorran Henrique Rezende Alves <72421034+lorranhenrique@users.noreply.github.com>
2025-01-26 15:34:46 -0300

    feat:Animação do botão de entrada

--- Bubble Gun/Assets/Scripts/UI/ChangeScene.cs
+++ Bubble Gun/Assets/Scripts/UI/ChangeScene.cs
@@ -1,10 +1,23 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] private Animator anim;
+    [SerializeField] private GameObject textButton;
     public void ChangeSceneByIndex(int sceneIndex)
     {
+        anim.SetTrigger("Clicked");
+        //textButton.SetActive(false);
+        StartCoroutine(ChangeSceneDelay(sceneIndex));
+        
+    }
+
+    IEnumerator ChangeSceneDelay(int sceneIndex)
+    {
+        yield return new WaitForSeconds(0.6f);
         SceneManager.LoadScene(sceneIndex);
+
     }
 }
